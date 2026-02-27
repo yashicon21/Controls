812fe40b8fb7047eae4b186c3d2c1dcd21c5be2d
Yashar Zolmajdi <49378236+yashicon21@users.noreply.github.com>
2026-02-27 13:06:42 +1100

    Fix button opacity when disabled

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default ({ children, disabled = false, onClick, type, value, className = "", Component = "button" }) => (
   <Component
     className={`bg-purple-700 text-white tracking-wider py-2 rounded w-full uppercase cursor-pointer ${className} ${
-      disabled ? "opacity-50 cursor-not-allowed" : "hover:bg-purple-800"
+      disabled ? "opacity-10 cursor-not-allowed" : "hover:bg-purple-800"
     }`}
     onClick={!disabled ? onClick : undefined}
     disabled={disabled}
